0e1d217dd118d2a0b452fb9f9dacca7747dc0b6a
mayukhdeep12 <mayukhdeep12mathur@yahoo.com>
2025-01-03 17:30:39 +0530

    Update white theme 3

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
               <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
                 <Hero />
                 <Collaboration />
-                <Services />
+                {/* <Services /> */}
                 <Roadmap />
                 
                 {/* <Carousel /> */}
